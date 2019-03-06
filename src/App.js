71fc2db555cde9072c3e89a3376412f8dd342fe6
Tentrilix <tentrilix@gmail.com>
2019-03-06 16:52:09 +0100

    added time

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function Welcome(property) { return (
 	<p>Hello {property.name}</p>
 );}
 
 function App() {
-	const hello = (
+	return (
 		<div>
-			<img src={logo} width={128}/>
-			<Welcome name="asd" />
-			<Welcome name="dsa" />
-			<Welcome name="fas" />
+			<img src={logo} width={128} alt="pic"/>
+			<Welcome name="asd"/>
+			<Welcome name="dsa"/>
+			<Welcome name="fas"/>
 		</div>
 	);
-	const time = (
-		<div>
-			<h1>{new Date().toLocaleTimeString()}</h1>
-		</div>
-	);
-
-	return hello;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import Time from "./Time";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+function Draw() {
+	return (
+		ReactDOM.render(<Time/>, document.getElementById("time"))
+	);
+}
+
+setInterval(Draw, 1000);
+
 serviceWorker.unregister(); //.register() ha offline is kéne használni
